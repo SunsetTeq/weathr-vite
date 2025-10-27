703d2ef3d138431a56b6ba4ecc2aee0a052199f5
SunsetTeq <dotadgager@gmail.com>
2025-10-27 14:22:55 +0300

    light template add

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@pages': '/src/pages',
+      '@widgets': '/src/widgets',
+      '@features': '/src/features',
+      '@entities': '/src/entities',
+      '@api': '/src/shared/api',
+      '@constants': '/src/shared/config/constants',
+      '@hooks': '/src/shared/lib/hooks',
+      '@utils': '/src/shared/lib/utils',
+      '@slices': '/src/shared/model/slices',
+      '@type': '/src/shared/model/types',
+      '@ui': '/src/shared/ui',
+      '@assets': '/src/shared/assets',
+    },
+  },
+});
