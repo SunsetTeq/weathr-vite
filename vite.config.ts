a5ce83261ff9e20839ad57fcd45da5adab879d39
SunsetTeq <dotadgager@gmail.com>
2025-11-20 14:48:44 +0300

    readme update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/weathr-vite/',
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
